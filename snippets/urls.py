49c2d252d63aa42c87bf51f34e18937800cfc351
Ammar Hussain <ammar@ideawake.com>
2021-06-24 17:02:45 +0500

    Adding HTML response for Highlighted Snippet

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import SnippetList, SnippetDetail, UserList, UserDetail, api_root
+from .views import SnippetHighlight, SnippetList, SnippetDetail, UserList, UserDetail, api_root
 
 urlpatterns = [
     path('snippets/', SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', SnippetHighlight.as_view(), name='snippet-highlight'),
     path('users/', UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
     path('', api_root),
