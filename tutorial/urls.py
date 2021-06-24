18253129cb9b4e86544c431cab929d0729aaef12
Ammar Hussain <ammar@ideawake.com>
2021-06-24 16:10:51 +0500

    Added Login to Browsable API

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('snippets.urls'))
 ]
