07556206ddc7cfa4a2a91713d61b0130cca64de0
Ammar Hussain <ammar@ideawake.com>
2021-06-24 14:03:11 +0500

    Add and Register Snippet List and Detail Views

---
+++ snippets/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import SnippetList, SnippetDetail
+
+urlpatterns = [
+    path('snippets/', SnippetList.as_view()),
+    path('snippets/<int:pk>/', SnippetDetail.as_view())
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+"""
+    Including format_suffix_patterns is an optional choice that provides a simple, 
+    DRY way to refer to a specific file format for a URL endpoint. 
+    It means our API will be able to handle URls such as http://example.com/api/items/4.json 
+    rather than just http://example.com/api/items/4.
+"""
